58731f10513b1591053e8446bcd5ba803351d221
Christian Di Lorenzo <rcddeveloper@icloud.com>
2019-03-18 14:38:16 -0400

    Add download script for ESV and Nave's

---
+++ download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FOLDER="data/"
+
+mkdir "$FOLDER"
+cd "$FOLDER"
+
+echo "Downloading ESV..."
+wget https://github.com/honza/bibles/raw/master/ESV/ESV.json
+
+echo "Downloading Nave's Topical Index..."
+wget http://www.justverses.com/downloads/zips/nave.zip
+mkdir nave
+unzip nave.zip -d nave
+rm nave.zip
