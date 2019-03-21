12869305f677c9ff7bfa921f257f2a4b4cda7788
Christian Di Lorenzo <rcddeveloper@icloud.com>
2019-03-21 18:34:59 -0400

    Add parsing TSV from client-side

--- client/src/models/nave.js
+++ client/src/models/nave.js
@@ -1,6 +1,7 @@
 import naveReference from '../data/nave-by-reference.json';
 import naveTopics from '../data/nave-by-topic.json';
 import * as R from 'ramda';
+import { tsv } from 'd3-fetch';
 
 export const topics = (book, chapter, verse) => {
   return Promise.resolve(naveReference[book][chapter][verse]);
@@ -9,6 +10,10 @@ export const topics = (book, chapter, verse) => {
 export const allTopics = () => {
   return Promise.resolve(Object.keys(naveTopics));
 };
+
+export const topicNodes    = () => tsv('/data/nave-topic-nodes.tsv');
+export const categoryNodes = () => tsv('/data/nave-category-nodes.tsv');
+export const subtopicNodes = () => tsv('/data/nave-subtopics-nodes.tsv');
 
 export const topicCounts = () => {
   const mapAsValues = f => R.pipe(Object.values, R.map(f))
