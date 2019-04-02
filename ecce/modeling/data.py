dcafa7cdf51d2de7506222bfbddd74d6391f7ea1
Christian Di Lorenzo <rcddeveloper@icloud.com>
2019-04-01 22:26:57 -0400

    Add basic train/test split for experimentation

--- ecce/modeling/data.py
+++ ecce/modeling/data.py
@@ -5,6 +5,7 @@ from ascii_graph import Pyasciigraph
 from funcy import flatten
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
 from toolz import memoize
 
 from ecce.constants import NLP_TOPICS_PATH
@@ -42,12 +43,20 @@ def topic_encoder():
     return encoder
 
 
-def print_topic_graph():
-    topic_counts = frame().topics.apply(len).value_counts().sort_index()
-    hist_data = [
-        (f'{count} topic{"" if count == 1 else "s"}', freq)
-        for count, freq in zip(topic_counts.index, topic_counts.values)
-    ]
+@memoize
+def data_split():
+    df = frame()
+    text = df.text.values
+    topics = topic_encoder().transform(df.topics.values)
+    return train_test_split(text, topics, test_size=0.25, random_state=1337)
 
+
+def topic_histogram():
+    topic_counts = frame().topics.apply(len).value_counts().sort_index()
+    return [(f'{count} topic{"" if count == 1 else "s"}', freq)
+            for count, freq in zip(topic_counts.index, topic_counts.values)]
+
+
+def print_topic_graph():
     graph = Pyasciigraph(line_length=40)
-    [print(l) for l in graph.graph('# Topics per Verse', hist_data)]
+    [print(l) for l in graph.graph('# Topics per Verse', topic_histogram())]
